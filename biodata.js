15661ee74b6322f8ca0e138ae0929b699a3a96fd
Gilang <kurbiagilank@gmail.com>
2026-07-06 19:28:36 +0700

    feat: biodata bang

---
+++ biodata.js
@@ -0,0 +1,11 @@
+// 1. Variabel dengan const (nilai tetap, tidak boleh diubah)
+const namaLengkap = "Gilang";
+const usia = 23;
+const kota = "Pekanbaru";
+
+// 2. Variabel dengan let (nilai boleh berubah)
+let moodHariIni = "Anjay";
+
+// 3. Tampilkan menggunakan template literal
+console.log(`Nama saya ${namaLengkap}, usia ${usia} tahun, tinggal di ${kota}.`);
+console.log(`Mood saya hari ini: ${moodHariIni}`);
